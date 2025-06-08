40194dfc97cba4fb412cb4320f1444ccbc205db0
LeeJihun <ljh09060@gmail.com>
2025-06-08 21:21:38 +0900

    feat: 데이터베이스에서 테이블을 가져오고, 로그 찍어보기 추가

--- shop/src/main/java/com/apple/shop/ItemController.java
+++ shop/src/main/java/com/apple/shop/ItemController.java
@@ -1,13 +1,23 @@
 package com.apple.shop;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
+
+    private final ItemRepository itemRepository;
     @GetMapping("/list")
     String list(Model model){
+        List<Item> result = itemRepository.findAll();
+        System.out.println(result.get(0).price);
+        System.out.println(result.get(0).title);
+        System.out.println(result);
         model.addAttribute("name", "기운이 넘친다 기운줄게");
         return "list.html";
     }
