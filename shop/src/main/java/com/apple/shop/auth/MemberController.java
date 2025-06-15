7dc90205a77854e25716d82f0f2dde4431aeff6e
Lee Jihun <ljh09060@gmail.com>
2025-06-15 23:46:40 +0900

    feat: 회원가입 데이터베이스 저장 추가

--- shop/src/main/java/com/apple/shop/auth/MemberController.java
+++ shop/src/main/java/com/apple/shop/auth/MemberController.java
@@ -1,6 +1,8 @@
 package com.apple.shop.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,13 +21,23 @@ public class MemberController {
 
     @PostMapping("/signUp/create")
     public String userCreate(String username, String password, String displayName) {
+        memberService.createMember(username,password,displayName);
         return "redirect:/list";
     }
 
     @GetMapping("/login")
     public String login() {
-        var result = memberRepository.findAllByUsername("hello");
-        System.out.println(result.get().getDisplayName());
+//        var result = memberRepository.findAllByUsername("hello");
+//        System.out.println(result.get().getDisplayName());
         return "login.html";
+    }
+
+    @GetMapping("/my-page")
+    public String myPage(Authentication auth) {
+        System.out.println(auth);
+        System.out.println(auth.getName()); //아이디출력가능
+        System.out.println(auth.isAuthenticated()); //로그인여부 검사가능
+//        System.out.println(auth.getAuthorities().contains(new SimpleGrantedAuthority("일반유저")));
+        return "mypage.html";
     }
 }
