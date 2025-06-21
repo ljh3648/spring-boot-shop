f6e55bfa5cc25237b34b7e1d87693d400007cdeb
LeeJihun <ljh09060@gmail.com>
2025-06-21 16:00:35 +0900

    refactor: 리포지토리 구성 변경

--- hello-spring/src/main/java/hello/hello_spring/controller/HelloController.java
+++
@@ -1,9 +0,0 @@
-//package hello.hello_spring.controller;
-//
-//public class HelloController {
-//    @GetMapping("hello")
-//    public String hello(Model mode){
-//        model.addAttribute(attributeName: "data", attributeValue: "hello!");
-//        return "hello";
-//    }
-//}
